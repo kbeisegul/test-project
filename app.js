c4a4604cbdb49e887e0b8716ba0dfe8036e38183
Бейсегул Куат <beisegul.k@magnum.kz>
2022-11-28 12:07:16 +0600

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ var infoRouter = require('./routes/info');
 var defaultRouter = require('./routes/default');
 
 var app = express();
+app.get('/', function (req, res) {
+  var name = req.query.name ? req.query.name: 'World'
+  res.send(`Hello, ${name}`)
+})
+
+// Define request response for about page
+app.get('/about', function (req, res) {
+  res.send(`<h3>About page is so cool</h3>`)
+})
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
